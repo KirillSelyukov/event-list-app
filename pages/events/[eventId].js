11af23c6b2ab2bded3ca8d5d25bb966d899b0445
Kyrylo Seliukov <kyrylo.seliukov@dxc.com>
2023-09-14 22:37:17 +0200

    Add event details

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,32 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 const EventDatailPage = () => {
-  return <div>welcome to EventDatailPage</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 };
 
 export default EventDatailPage;
